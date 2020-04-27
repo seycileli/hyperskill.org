8daa0ada92f16f8a865a5d5fe7cca97d527b50e1
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-04-27 17:43:31 +0300

    Create readingIntegerNumbers.java

---
+++ Java/basicSyntaxAndSimplePrograms/readingIntegerNumbers/readingIntegerNumbers.java
@@ -0,0 +1,23 @@
+package basicSyntaxAndSimplePrograms;
+
+import java.util.Scanner;
+
+public class readingIntegerNumbers {
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        solution();
+
+    }
+
+    private static void solution() {
+
+        System.out.println(sc.nextInt());
+        System.out.println(sc.nextInt());
+        System.out.println(sc.nextInt());
+        System.out.println(sc.nextInt());
+
+    }
+
+}
